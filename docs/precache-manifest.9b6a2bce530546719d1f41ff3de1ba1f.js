ce534560b7734180af42e8b435e69fecef0cecc8
Claudio Palma López <xclaudio@outlook.com>
2020-07-25 13:34:00 -0400

    docs agregado

---
+++ docs/precache-manifest.9b6a2bce530546719d1f41ff3de1ba1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8020e0f2e1cbab8ff4df494e88ecbf08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661b211f9a7d2e6af00d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661b211f9a7d2e6af00d",
+    "url": "/static/js/main.89a3ec89.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
